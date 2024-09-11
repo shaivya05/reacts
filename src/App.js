7fb168a8951a75624119a074925ceaac3e7d420c
shaivya05 <130532388+shaivya05@users.noreply.github.com>
2024-09-12 01:57:16 +0530

    created the react page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>hello world form react</h1>
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="red"
+          ariaLabel="loading"
+          wrapperStyle
+          wrapperClass
+        />
       </header>
     </div>
   );
